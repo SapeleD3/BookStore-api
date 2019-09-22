0882683410734120286999c9bb397998d4714c22
sapeled3 <oyinkuromosesvictor@outlook.com>
2019-09-22 22:23:16 +0100

    heroku deploy setup complte

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://MoSalaH:Slugterra5@cluster0-6sbfo.mongodb.net/test?retryWrites=true&w=majority',
+    googleClientID: '584811727710-mjf58anjunpm2nkk2cqo4gbqvdcuqe57.apps.googleusercontent.com',
+    googleClientSecret: '9qD1j-BL36nIxjUCOBAt1uQW'
+}
