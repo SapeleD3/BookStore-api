fd9ba5ede2f1690dc61096db20d38add25a62500
sapeled3 <oyinkuromosesvictor@outlook.com>
2019-09-22 23:13:08 +0100

    create user model

--- server.js
+++ server.js
@@ -2,8 +2,15 @@ const express = require('express');
 const mongoose = require('mongoose')
 const app = express();
 const passport = require('passport')
+const keys = require('./config/keys')
 
 require('./config/passport')(passport)
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
 
 const auth = require('./routes/auth')
 
