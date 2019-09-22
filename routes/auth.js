daa346579c86ffe936c655f22e9202665365459b
sapeled3 <oyinkuromosesvictor@outlook.com>
2019-09-22 22:46:42 +0100

    handled auth

--- routes/auth.js
+++ routes/auth.js
@@ -4,4 +4,12 @@ const passport = require('passport')
 
 router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
 
+router.get('/google/callback',
+    passport.authenticate('google', { failureRedirect: '/' }),
+    (req, res) => {
+        // Successful authentication, redirect home.
+        res.redirect('/dashboard');
+    }
+);
+
 module.exports = router;
