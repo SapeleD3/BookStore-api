7d5f58b58dd45dac743acfc32fc789cab95ad59d
sapeled3 <oyinkuromosesvictor@outlook.com>
2019-09-23 08:45:12 +0100

    o

--- routes/auth.js
+++ routes/auth.js
@@ -8,7 +8,7 @@ router.get('/google/callback',
     passport.authenticate('google', { failureRedirect: '/' }),
     (req, res) => {
         // Successful authentication, redirect home.
-        res.redirect('/dashboard');
+        res.redirect('/');
     }
 );
 
